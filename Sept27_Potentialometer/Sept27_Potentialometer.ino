5cd5aa7338728adf45e62c2a1446a38fb3f4257a
jasontan5126 <59143666+jasontan5126@users.noreply.github.com>
2021-09-27 12:58:21 -0400

    Update Sept27_Potentialometer.ino

--- Sept27_Potentialometer/Sept27_Potentialometer.ino
+++ Sept27_Potentialometer/Sept27_Potentialometer.ino
@@ -1,18 +1,20 @@
+//Potentiometer to 
+
 #define RECEIVER 14
 #define LED 7
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED, OUTPUT);    //To receive the output at 14
-  pinMode(RECEIVER, INPUT);   //To read the input at 7
+  pinMode(LED, OUTPUT);    //To receive the output at A0
+  pinMode(RECEIVER, INPUT);   //To read the input at PD7
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogRead(RECEIVER);
-  digitalWrite(LED, HIGH);
-  delay(analogRead(RECEIVER));
-  digitalWrite(LED, LOW);
+  analogRead(RECEIVER);    //Reads the value from the specified analog pin
+  digitalWrite(LED, HIGH); //Set the LED output to high
+  delay(analogRead(RECEIVER));  //delay by small time
+  digitalWrite(LED, LOW); //set the LED output to low
   delay(analogRead(RECEIVER));
 }
